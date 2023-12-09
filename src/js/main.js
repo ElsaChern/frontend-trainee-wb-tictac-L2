00fa38784b25265d221f1ed7cd70bf073498172a
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-12-10 00:17:16 +0300

    add hard level for computer

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const newGame = document.querySelector(".new-game");
 
 let gameDifficulty = "0";
 let gameFormat = "twoPlayers";
+let humanPlayer = "x";
+let computerPlayer = "o";
 
 difficulty.forEach((item) => {
   item.addEventListener("change", () => {
@@ -43,24 +45,65 @@ const win = [
 
 statusTxt.textContent = `${player} твой ход`;
 
-const makeAMove = (cell) => {
-  let innerHTML = document.createElement("div");
-  innerHTML.classList.add(player);
-  innerHTML.textContent = player;
-  cell.appendChild(innerHTML);
+const miniMax = (field, player) => {
+  let emptyCells = field.filter((item) => item === "");
 
-  checkWin();
-
-  if (gameEnded) {
-    return;
+  if (isPlayerWon(field, humanPlayer)) {
+    return { score: -10 };
+  } else if (isPlayerWon(field, computerPlayer)) {
+    return { score: 10 };
+  } else if (emptyCells.length === 0) {
+    return { score: 0 };
   }
 
-  player = player === "x" ? "o" : "x";
-  statusTxt.textContent = `${player} твой ход`;
+  let moves = [];
+
+  for (let i = 0; i < field.length; i++) {
+    if (field[i] !== "") {
+      continue;
+    }
+
+    let currentMove = {};
+
+    currentMove.index = i;
+    field[i] = player;
+
+    let nextPlayer = player === "x" ? "o" : "x";
+    let result = miniMax(field, nextPlayer);
+    currentMove.score = result.score;
+
+    field[i] = "";
+    moves.push(currentMove);
+  }
+
+  let bestMove;
+  if (player === computerPlayer) {
+    let bestScore = -10000;
+
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    let bestScore = 10000;
+
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  return moves[bestMove];
 };
 
 const theBestMove = () => {
-  let emptyCells = Array.from(cells).filter((cell) => cell.innerHTML === "");
+  let field = Array.from(cells).map((cell) => cell.textContent);
+
+  let result = miniMax(field, player);
+  return cells[result.index];
 };
 
 const randomMove = () => {
@@ -78,6 +121,22 @@ const computerChoise = () => {
   }
 };
 
+const makeAMove = (cell) => {
+  let innerHTML = document.createElement("div");
+  innerHTML.classList.add(player);
+  innerHTML.textContent = player;
+  cell.appendChild(innerHTML);
+
+  checkWin();
+
+  if (gameEnded) {
+    return;
+  }
+
+  player = player === "x" ? "o" : "x";
+  statusTxt.textContent = `${player} твой ход`;
+};
+
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
     if (gameEnded || cell.innerHTML !== "") {
@@ -86,11 +145,30 @@ cells.forEach((cell) => {
 
     makeAMove(cell);
 
-    if (gameFormat === "computer") {
+    if (!gameEnded && gameFormat === "computer") {
       makeAMove(computerChoise());
     }
   });
 });
+
+const isPlayerWon = (field, player) => {
+  for (let i = 0; i < win.length; i++) {
+    if (
+      field[win[i][0]] === player &&
+      field[win[i][1]] === player &&
+      field[win[i][2]] === player
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const gameOver = (isDraw = false) => {
+  gameEnded = true;
+  statusTxt.textContent = isDraw ? "Ничья!" : `Победа ${player}! `;
+  newGame.style.display = "inline";
+};
 
 const checkWin = () => {
   for (let i = 0; i < win.length; i++) {
@@ -102,22 +180,15 @@ const checkWin = () => {
       document.getElementById(win[i][0]).children[0].classList.add("win-color");
       document.getElementById(win[i][1]).children[0].classList.add("win-color");
       document.getElementById(win[i][2]).children[0].classList.add("win-color");
-      gameEnded = true;
 
-      // тут мб стоит указать, чья победа? Типо statusTxt.textContent = `Победа ${player}!`;
-      statusTxt.textContent = `Победа ${player}! `;
-
-      newGame.style.display = "inline";
+      gameOver();
     }
   }
 
-  let isCellsFilled = Array.from(cells).map((cell) =>
-    ["x", "o"].includes(cell.textContent),
-  );
+  let emptyCells = Array.from(cells).filter((cell) => cell.textContent === "");
 
-  if (!gameEnded && !isCellsFilled.includes(false)) {
-    gameEnded = true;
-    statusTxt.textContent = `Ничья!`;
+  if (!gameEnded && emptyCells.length === 0) {
+    gameOver(true);
   }
 };
 
@@ -130,4 +201,5 @@ newGame.addEventListener("click", () => {
   });
 
   statusTxt.textContent = `${player} твой ход`;
+  gameOptions.style.display = "block";
 });
